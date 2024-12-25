595af37dc91a1bc6b2b0012280809b49415d803c
minhnguyen1502 <minhnguyen010502@gmail.com>
2024-12-25 17:31:49 +0700

    first commit

--- app/src/main/java/com/example/api/model/Item.kt
+++ app/src/main/java/com/example/api/model/Item.kt
@@ -1,5 +1,6 @@
 package com.example.api.model
 
+// food
 data class FoodResponse(
     val code: Int,
     val msg: String,
@@ -17,7 +18,7 @@ data class FoodItem(
     val image: String,
     val price: Int
 )
-
+// drink
 data class DrinkResponse(
     val code: Int,
     val msg: String,
@@ -37,7 +38,7 @@ data class DrinkItem(
     val discount: Int,
     val percent_discount: Int
 )
-
+// location
 data class LocationResponse(
     val code: Int,
     val msg: String,
@@ -54,6 +55,7 @@ data class LocationItem(
     val name: String,
     val distance: String
 )
+// order
 data class Order(
     val id: Int,
     val name: String,
@@ -65,9 +67,48 @@ data class OrderResponse(
     val msg: String,
     val data: String
 )
+// cancel order
 data class DeleteResponse(
     val code: Int,
     val msg: String,
     val data: String
 )
+// drink detail
+
+data class DrinkItemResponse(
+    val code: Int,
+    val msg: String,
+    val dat: DrinkDetail?
+)
+
+data class DrinkDetail(
+    val id:Int,
+    val name: String,
+    val price: Int,
+    val size: String,
+    val discount: Int,
+    val percent_discount: Int,
+    val image: String,
+    val ship_price: Int,
+    val payment_channel: String
+)
+// food detail
+data class FoodItemResponse(
+    val code: Int,
+    val msg: String,
+    val data: FoodDetail?
+)
+
+data class FoodDetail(
+    val id: Int,
+    val name: String,
+    val price: Int,
+    val ship_price: Int,
+    val image: String,
+    val payment_channel: String,
+    val percent_discount: Int,
+    val discount: Int
+)
+
+// history order
 
